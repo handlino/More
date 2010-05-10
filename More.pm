b4f2a04beb472402e9a8e6cc766b76e1369265b2
Kang-min Liu <gugod@gugod.org>
2010-05-10 22:23:46 +0800

    Corpus switching

--- More.pm
+++ More.pm
@@ -1,7 +1,9 @@
 package More;
 use Dancer;
+use strict;
+
 use JSON qw(to_json);
-use Acme::Lingua::ZH::Remix;
+use Acme::Lingua::ZH::Remix 0.90;
 use Encode qw(encode_utf8);
 
 get '/' => sub {
@@ -12,16 +14,38 @@ get '/api' => sub {
     template 'api';
 };
 
+my %remixer = (
+    default => Acme::Lingua::ZH::Remix->new
+);
+
 get '/sentences.json' => sub {
     my $self = shift;
     my $n = params->{n} || 1;
     my $cb = params->{callback};
+    my $corpus = params->{corpus} || "default";
+
+    my $remixer = $remixer{$corpus};
+    unless($remixer) {
+        my $corpus_file = Dancer::Config::setting("appdir") . "/corpus/${corpus}.txt";
+        if (-f $corpus_file) {
+            open(FH, "<:utf8", $corpus_file);
+            local $/ = undef;
+            my $text = <FH>;
+            close(FH);
+
+            $remixer{$corpus} = Acme::Lingua::ZH::Remix->new;
+            $remixer{$corpus}->feed($text);
+
+            $remixer = $remixer{$corpus};
+        }
+        else {
+            $remixer = $remixer{default};
+        }
+    }
+
 
     $n = 1 if $n > 100;
-
-    my @sentences;
-
-    push(@sentences, rand_sentence) while($n-->0);
+    my @sentences = map { $remixer->random_sentence } 1..$n;
 
     my $json_text = to_json({ sentences => \@sentences });
 
