d05148f29eb29657582c0fcbaee52f9fe48f8f91
Kang-min Liu <gugod@gugod.org>
2010-05-10 23:11:24 +0800

    Pre-process the grabbed tweets

--- bin/grab-tweets.pl
+++ bin/grab-tweets.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/env perl
 use common::sense;
+use utf8;
+
 use Net::Twitter;
 use Encode::HanConvert qw(simp_to_trad);
+use Acme::Lingua::ZH::Remix;
 use YAML;
+use List::MoreUtils qw(uniq);
 
 sub grab_tweets {
     my ($keyword, $since_id) = @_;
@@ -10,13 +14,17 @@ sub grab_tweets {
     my $r;
     my @tweets;
 
-    for (1..10) {
-        $r = $t->search({ q => $keyword, page => 11 - $_, since_id => $since_id });
+    for (1..5) {
+        say "  - page $_";
+
+        $r = $t->search({ q => $keyword, page => $_, since_id => $since_id });
         last unless @{$r->{results}};
 
-        push @tweets, map { simp_to_trad($_) } grep { !/(http|@\S+)/ } map { s{\s+http://plurk.com/p/.+$}{}; $_ } map { $_->{text} } @{$r->{results}};
+        push @tweets, grep { $_ } map { join "", Acme::Lingua::ZH::Remix->split_corpus($_) } map { simp_to_trad($_) } grep { !/(http|@\S+)/ } map { s{\s+http://plurk.com/p/.+$}{}; $_ } map { $_->{text} } @{$r->{results}};
     }
 
+    @tweets = uniq sort @tweets;
+    say "    - $_" for @tweets;
     return {
         max_id => $r->{max_id},
         tweets => \@tweets
@@ -24,11 +32,18 @@ sub grab_tweets {
 }
 
 my $config_file = "logs/tweets.yml";
-my $config = -f $config_file ? YAML::LoadFile($config_file) : [{ q => "不賴" }];
+my $config = -f $config_file ? YAML::LoadFile($config_file) : [{ q => "不賴", since_id => 1 }];
 
+for my $q (@ARGV) {
+    utf8::decode($q) unless utf8::is_utf8($q);
+
+    unshift @$config, { q => $q, since_id => 1 }
+}
 
 open CORPUS, ">>", "corpus/tweets.txt";
 for my $c (@$config) {
+    say "Grab Tweets for $c->{q}";
+
     my $r = grab_tweets($c->{q}, $c->{since_id});
     $c->{since_id} = $r->{max_id};
     say CORPUS $_ for @{$r->{tweets}};
@@ -36,4 +51,3 @@ for my $c (@$config) {
 close CORPUS;
 
 YAML::DumpFile($config_file, $config);
-
