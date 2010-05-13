fec5043db6d8fbaa11ca7ff7ae3f507287076217
Kang-min Liu <gugod@gugod.org>
2010-05-13 11:13:39 +0800

    Let the program fetch more pages of plurks by providing credentials

--- bin/grab-plurks-nextmedia.pl
+++ bin/grab-plurks-nextmedia.pl
@@ -1,35 +1,49 @@
 #!/usr/bin/env perl
 use common::sense;
-
-# http://www.plurk.com/m/u/TW_nextmedia
-use WWW::Mechanize::Cached;
+use WWW::Mechanize;
 use HTML::TreeBuilder::Select;
+use YAML 'LoadFile';
+use Getopt::Std;
+
+my %opts;
+getopt('c', \%opts);
+die "Usage: $0 -c plurk.yml\n" unless $opts{c};
+
+my $config = LoadFile($opts{c}) or die "Fail to load $opts{c}.yml\n";
 
 my @corpus;
 
-my $mech = WWW::Mechanize::Cached->new;
+my $mech = WWW::Mechanize->new;
+$mech->agent_alias( 'Mac Mozilla' );
+
+$mech->get("http://www.plurk.com/m/login");
+$mech->submit_form(with_fields => {username => $config->{username}, password => $config->{password}});
 
 $mech->get("http://www.plurk.com/m/u/TW_nextmedia");
+say $mech->content;
 
-my $tree = HTML::TreeBuilder::Select->new;
-$tree->parse($mech->content);
-my @plurks = $tree->select(".plurk");
+for(1..10) {
+    my $tree = HTML::TreeBuilder::Select->new;
+    $tree->parse($mech->content);
+    my @plurks = $tree->select(".plurk");
 
-
-for (@plurks) {
-    my @children = $_->content_list;
-    shift @children;
-    shift @children;
-    pop @children;
-
-    if ($children[0]->tag eq 'span') {
+    for (@plurks) {
+        my @children = $_->content_list;
         shift @children;
+        shift @children;
+        pop @children;
+
+        if ($children[0]->tag eq 'span') {
+            shift @children;
+        }
+
+        my @p = map { join("，", split(" ", $_)) } map { s{(?<![[:punct:]])$}{。}; $_} grep { s/^\s+//g;s/\s+$//g; $_ } grep { !/(tw\.img\.nextmedia\.com|\*\*.+\*\*)/ } map { ref($_) ? $_->as_trimmed_text : $_ } @children;
+        say "=> $_" for @p;
+        push @corpus, @p;
     }
 
-    push @corpus, map { join("，", split(" ", $_)) } map { s{(?<![[:punct:]])$}{。}; $_} grep { s/^\s+//g;s/\s+$//g; $_ } grep { !/(tw\.img\.nextmedia\.com|\*\*.+\*\*)/ } map { ref($_) ? $_->as_trimmed_text : $_ } @children;
+    $mech->follow_link(text_regex => qr/next /);
 }
-
-$mech->follow_link(url_regex => qr/(?i:offset=\d+$)/);
 
 open OUT, ">>:utf8", "corpus/nextmedia.txt";
 say OUT $_ for @corpus;
