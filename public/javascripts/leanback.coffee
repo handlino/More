c8a12eaf562409d461df7177d5b9cc1bbb461298
Kang-min Liu <gugod@gugod.org>
2012-02-26 22:32:56 +0800

    first /leanback page implementation

--- public/javascripts/leanback.coffee
+++ public/javascripts/leanback.coffee
@@ -1,10 +1,36 @@
-change_background = ->
-    url = location.host + "/pictures/" + parseInt(Math.random()*1000000000000).toString(16) + "/960x960.jpg";
-    img = new Image(url);
-    img.onload = ->
-        $("body").css("background-image", "url(" + url + ")")
-    $("#loader").append(img);
+image_loaded = false
+text_loaded = false
+
+change = ->
+    console.log("change")
+    if (text_loaded && image_loaded)
+        $("#screen").css("background-image", "url(" + $("#loader img").attr("src") + ")")
+        $("#loader img").remove()
+        $("#screen p").html( $("#loader p").html() )
+        image_loaded = false
+        text_loaded = false
+
+    else if (!text_loaded && !image_loaded)
+        load_text()
+        load_background()
+
+    setTimeout change, 3000
+
+load_text = ->
+    $("#loader p").moreText { 'n': 3, 'max': 45, 'callback': (sentences) ->
+        $(this).html(sentences.join("<br>"))
+        text_loaded = true
+    }
+
+load_background = ->
+    url = location.protocol + "//" + location.host + "/pictures/" + parseInt(Math.random()*1000000000000).toString(16) + "/960x960.jpg";
+    img = new Image();
+    $(img).on "load", ->
+        image_loaded = true
+    $("#loader").append(img)
+    img.src = url
 
 jQuery ->
-setTimeout change_background, 10000
+    jQuery.moreText.server = location.protocol + "//" + location.host
+    change()
 
--- public/javascripts/leanback.js
+++ public/javascripts/leanback.js
@@ -1,18 +1,50 @@
 (function() {
-  var change_background;
+  var change, image_loaded, load_background, load_text, text_loaded;
 
-  change_background = function() {
-    var img, url;
-    url = location.host + "/pictures/" + parseInt(Math.random() * 1000000000000).toString(16) + "/960x960.jpg";
-    img = new Image(url);
-    img.onload = function() {
-      return $("body").css("background-image", "url(" + url + ")");
-    };
-    return $("#loader").append(img);
+  image_loaded = false;
+
+  text_loaded = false;
+
+  change = function() {
+    console.log("change");
+    if (text_loaded && image_loaded) {
+      $("#screen").css("background-image", "url(" + $("#loader img").attr("src") + ")");
+      $("#loader img").remove();
+      $("#screen p").html($("#loader p").html());
+      image_loaded = false;
+      text_loaded = false;
+    } else if (!text_loaded && !image_loaded) {
+      load_text();
+      load_background();
+    }
+    return setTimeout(change, 3000);
   };
 
-  jQuery(function() {});
+  load_text = function() {
+    return $("#loader p").moreText({
+      'n': 3,
+      'max': 45,
+      'callback': function(sentences) {
+        $(this).html(sentences.join("<br>"));
+        return text_loaded = true;
+      }
+    });
+  };
 
-  setTimeout(change_background, 10000);
+  load_background = function() {
+    var img, url;
+    url = location.protocol + "//" + location.host + "/pictures/" + parseInt(Math.random() * 1000000000000).toString(16) + "/960x960.jpg";
+    img = new Image();
+    $(img).on("load", function() {
+      return image_loaded = true;
+    });
+    $("#loader").append(img);
+    return img.src = url;
+  };
+
+  jQuery(function() {
+    jQuery.moreText.server = location.protocol + "//" + location.host;
+    return change();
+  });
 
 }).call(this);
