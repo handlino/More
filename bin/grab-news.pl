67b6587f5d44d9f196885be17f625f642a5a4a04
Kang-min Liu <gugod@gugod.org>
2012-02-27 08:17:18 +0800

    grab multiple sections

--- bin/grab-news.pl
+++ bin/grab-news.pl
@@ -12,18 +12,23 @@ use List::MoreUtils qw(uniq);
 sub fetch_news_titles {
     my @titles;
 
-    my $feed = XML::Feed->parse(URI->new('http://news.google.com.tw/news?output=rss'))
-        or die XML::Feed->errstr;
+    for my $topic (qw(t y w n b s e c m)) {
+        my $uri = URI->new("http://news.google.com.tw/news?topic=${topic}&output=rss");
 
-    for my $entry ($feed->entries) {
-        push @titles, $entry->title =~ s/ - .+?$//r =~ s/[_ ]/，/gr =~ s/$/。/r;
+        my $feed = XML::Feed->parse($uri) or next;
+        for my $entry ($feed->entries) {
+            my $t = $entry->title =~ s/ - .+?$//r =~ s/[_ ]/，/gr =~ s/$/。/r;
+
+            push @titles, $t;
+        }
     }
 
     return @titles;
 }
 
 my @old_titles = io("corpus/news.txt")->assert->utf8->chomp->getlines;
-my @titles = uniq sort fetch_news_titles(), @old_titles;
+my @new_titles = fetch_news_titles();
+my @titles = uniq sort @new_titles, @old_titles;
 
 my $out = io("corpus/news.txt")->utf8;
 $out->println($_) for @titles;
