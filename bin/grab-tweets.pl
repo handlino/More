a4a5b914691fc301baeb219e0609ff4060d66c95
Kang-min Liu <gugod@gugod.org>
2012-02-27 09:13:09 +0800

    rewrite

--- bin/grab-tweets.pl
+++ bin/grab-tweets.pl
@@ -2,38 +2,44 @@
 use common::sense;
 use utf8;
 
-use Net::Twitter;
+# use Acme::Lingua::ZH::Remix;
 use Encode::HanConvert qw(simp_to_trad);
-use Acme::Lingua::ZH::Remix;
-use YAML;
-use List::MoreUtils qw(uniq);
 use HTML::Entities;
+use IO::All;
+use List::MoreUtils qw(uniq);
+use Net::Twitter;
+use URI;
+use XML::Feed;
+use YAML;
+
+my @dirs = io->catfile(__FILE__)->absolute->splitdir();
+splice @dirs, -2;
+
+my $app_root = io->catdir(@dirs);
 
 sub grab_tweets {
     my ($keyword, $since_id) = @_;
-    my $t = Net::Twitter->new(traits => ['API::Search']);
+    my $t = Net::Twitter->new(traits => ['API::REST', 'API::Search']);
     my $r;
     my @tweets;
 
     for (1..5) {
-        say "  - page $_";
-
         $r = $t->search({ q => $keyword, page => $_, since_id => $since_id });
         last unless @{$r->{results}};
 
-        push @tweets, grep { $_ } map { join "", Acme::Lingua::ZH::Remix->split_corpus($_) } map { s/!+/！/g; s/\?+/？/g; s/,+/，/g; s{(?<![[:punct:]])$}{。}; $_ } map { decode_entities($_) }  map { simp_to_trad($_) } grep { !/(http|@\S+)/ } map { s{^.+?\s(.+)\s+http://plurk.com/p/.+$}{$1}; $_ } map { $_->{text} } @{$r->{results}};
+        push @tweets, grep { $_ } map { s/!+/！/g; s/\?+/？/g; s/,+/，/g; s{(?<![[:punct:]])$}{。}; $_ } map { decode_entities($_) }  map { simp_to_trad($_) } grep { !/(http|@\S+)/ } map { s{^.+?\s(.+)\s+http://plurk.com/p/.+$}{$1}; $_ } map { $_->{text} } @{$r->{results}};
     }
 
     @tweets = uniq sort @tweets;
-    say "    - $_" for @tweets;
+
     return {
         max_id => $r->{max_id},
         tweets => \@tweets
     }
 }
 
-my $config_file = "logs/tweets.yml";
-my $config = -f $config_file ? YAML::LoadFile($config_file) : [{ q => "不賴", since_id => 1 }];
+my $config_file = $app_root->catfile("logs", "tweets.yml");
+my $config = -f $config_file ? YAML::LoadFile($config_file) : [{ q => "不賴", since_id => 1 }, { q => "咖啡", since_id => 1 }];
 
 for my $q (@ARGV) {
     utf8::decode($q) unless utf8::is_utf8($q);
@@ -41,14 +47,31 @@ for my $q (@ARGV) {
     unshift @$config, { q => $q, since_id => 1 }
 }
 
-open CORPUS, ">>", "corpus/tweets.txt";
-for my $c (@$config) {
-    say "Grab Tweets for $c->{q}";
+my @new_tweets;
 
+for my $c (@$config) {
     my $r = grab_tweets($c->{q}, $c->{since_id});
     $c->{since_id} = $r->{max_id};
-    say CORPUS $_ for @{$r->{tweets}};
+    push @new_tweets, @{$r->{tweets}};
 }
-close CORPUS;
 
 YAML::DumpFile($config_file, $config);
+
+my @old_tweets = $app_root->catfile("corpus", "tweets.txt")->assert->utf8->chomp->getlines;
+
+if (@new_tweets + @old_tweets > 1024) {
+    my $n = 1024 - @new_tweets;
+    $n = 0 if ($n < 0);
+    @old_tweets = @old_tweets[ 0 .. $n ];
+}
+
+my @tweets = uniq sort @new_tweets, @old_tweets;
+
+if (@tweets > 1024) {
+    my $n = @tweets - 1024;
+    @old_tweets = @old_tweets[0..$#old_tweets-$n];
+    @tweets = uniq sort @new_tweets, @old_tweets;
+}
+
+my $out = $app_root->catfile("corpus", "tweets.txt")->utf8;
+$out->println($_) for @tweets;
